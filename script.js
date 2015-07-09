9113078ebca78e67a97def151e202897893ca386
Daniel Llinas <danllinas@gmail.com>
2015-07-09 17:15:41 -0400

    Added initial files for Wyncode exercise.

--- script.js
+++ script.js
@@ -3,12 +3,29 @@ var template = function(text) {
 };
 
 var main = function() {
-  $('form').submit(function() {
-      
-    
-    
+
+  //adds new item on submit
+  $('form').on('submit', function() {
+    var text = $("#todo").val(), 
+        html = template(text); 
+
+    $('.list').append(html);
+    $("#todo").val(""); 
+
     return false;  
-  }
+  });
+
+  //toggle star color. JS isn't being added to the added list items.
+  //how to fix that? start the search at the parent list instead of the star icon!
+  //Then use the added selector inside the on options. 
+  $('.list').on('click', '.glyphicon-star', function(){
+    $(this).toggleClass('active');
+  });
+
+  //remove the element
+  $('.list').on('click', '.glyphicon-remove', function(){
+    $(this).parent().remove(); 
+  });
   
   
 };
